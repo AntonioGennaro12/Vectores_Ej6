3e10235479c983871ba3c4917f1975488584e1df
Antonio Gennaro <antgennaro@hotmail.com>
2023-05-22 16:06:16 -0300

    Vectores Ej 6

--- vectores_ej6.js
+++ vectores_ej6.js
@@ -1,6 +1,6 @@
 const   miDiv1  = document.querySelector ("#mi-div1");
 
-const titulos = ["Titulo 1", "Titulo 2", "Titulo 3", "Titulo 4", "Titulo 5"];
+const titulos = ["Palabra 1", "Palabra 2", "Palabra 3", "Palabra 4", ""Palabra 5"];
 
 for (let cont=0; cont < titulos.length; cont++) {
     miDiv1.innerHTML += `<h2>${titulos[cont]}</h2>`;
